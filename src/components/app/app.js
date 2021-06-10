1908237cc66776fe23f6904af64817dc08da9c88
Yunusxon Egamberdiev <yunusxon@gmail.com>
2021-06-10 18:25:17 +0500

    Add

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
@@ -28,4 +27,4 @@ const App = () => {
 	);
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
