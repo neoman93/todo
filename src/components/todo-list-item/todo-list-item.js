febe93442093052a35a00534cf11b130791631a2
Yunusxon Egamberdiev <yunusxon@gmail.com>
2021-06-12 18:28:02 +0500

    add

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./todo-list-item.css";
 
-import './todo-list-item.css';
+class TodoListItem extends Component {
+	onLabelClick = () => {
+		console.log(`Done: ${this.props.label}`);
+	};
+	render() {
+		const { label, important = false } = this.props;
 
-const TodoListItem = ({ label, important = false }) => {
+		const style = {
+			color: important ? "steelblue" : "black",
+			fontWeight: important ? "bold" : "normal",
+		};
+		return (
+			<div>
+				<span className="todo-list-item">
+					<span className="todo-list-item-label" style={style} onClick={this.onLabelClick}>
+						{label}
+					</span>
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+					<button type="button" className="btn btn-outline-success btn-sm float-right">
+						<i className="fa fa-exclamation" />
+					</button>
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
-      </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+					<button type="button" className="btn btn-outline-danger btn-sm float-right">
+						<i className="fa fa-trash-o" />
+					</button>
+				</span>
+			</div>
+		);
+	}
+}
 
 export default TodoListItem;
